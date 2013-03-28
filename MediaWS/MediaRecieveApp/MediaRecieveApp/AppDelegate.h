626044cd17f38960c1ee4f38a7f6ecbc34d79959
daviad <592597406@qq.com>
2013-03-28 20:20:31 +0800

    xx

---
+++ MediaWS/MediaRecieveApp/MediaRecieveApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MediaRecieveApp
+//
+//  Created by ding xiuwei on 13-3-27.
+//  Copyright (c) 2013年 ding xiuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
