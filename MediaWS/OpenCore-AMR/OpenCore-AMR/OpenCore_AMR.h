62682e548bf6c7b4b9be5ff43c0b53ccf88d97af
daviad <592597406@qq.com>
2013-04-04 16:42:08 +0800

    opencor

---
+++ MediaWS/OpenCore-AMR/OpenCore-AMR/OpenCore_AMR.h
@@ -0,0 +1,13 @@
+//
+//  OpenCore_AMR.h
+//  OpenCore-AMR
+//
+//  Created by dxw on 13-4-4.
+//  Copyright (c) 2013年 daviad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCore_AMR : NSObject
+
+@end
---
+++ MediaWS/OpenCore-AMR/OpenCore-AMR/OpenCore_AMR.m
@@ -0,0 +1,13 @@
+//
+//  OpenCore_AMR.m
+//  OpenCore-AMR
+//
+//  Created by dxw on 13-4-4.
+//  Copyright (c) 2013年 daviad. All rights reserved.
+//
+
+#import "OpenCore_AMR.h"
+
+@implementation OpenCore_AMR
+
+@end
