51adcea27dccbd77d7035f4854d1656848fc7796
daviad <592597406@qq.com>
2013-05-13 20:01:46 +0800

    s

--- MediaWS/MediaSenderApp/MediaSenderApp/AppDelegate.mm
+++ MediaWS/MediaSenderApp/MediaSenderApp/AppDelegate.mm
@@ -62,6 +62,27 @@ using namespace std;
 using namespace jrtplib;
 
 
+
+static void CheckError(OSStatus error, const char *operation)
+{
+    if (error == noErr) return;
+    
+    char str[20];
+    // see if it appears to be a 4-char-code
+    *(UInt32 *)(str + 1) = CFSwapInt32HostToBig(error);
+    if (isprint(str[1]) && isprint(str[2]) && isprint(str[3]) && isprint(str[4]))
+    {
+        str[0] = str[5] = '\'';
+        str[6] = '\0';
+    } else
+        // no, format it as an integer
+        sprintf(str, "%d", (int)error);
+    
+    fprintf(stderr, "Error: %s (%s)\n", operation, str);
+    
+    exit(1);
+}
+
 void HexOutput(const char* buf, size_t len)
 {
     printf("The Hex output of data :\n");
